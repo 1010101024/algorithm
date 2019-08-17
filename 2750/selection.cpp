258bcf320974710f39738864eba08d1a3a7ff1bd
512gb <50459446+512gb@users.noreply.github.com>
2019-08-17 23:56:03 +0900

    Add files via upload

---
+++ 2750/selection.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int n;
+	cin >> n;
+
+	int *a = new int [n];
+	for (int i=0; i < n; i++) {
+		cin >> a[i];
+	}
+	
+	int key;
+
+	for (int i = 1; i < n; i++) {		
+		key = a[i];
+		int j;
+		for (j = i - 1; j >= 0; j--) {
+			if (a[j] > key) {
+				a[j + 1] = a[j];
+			}
+			else {
+				break;
+			}
+	}
+		a[j + 1] = key;
+
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << endl;
+	}
+	delete[]a;
+	return 0;
+}
